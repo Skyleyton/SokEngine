c36b890924d7d59389b7f1a5a3140ad7cf758ab4
Skyleyton <dewenbidois@gmail.com>
2025-07-20 23:40:01 +0400

    Batch script for shader generation

--- generate_shader.bat
+++ generate_shader.bat
@@ -1,3 +1,5 @@
+@echo off
+
 .\tools\sokol-shdc.exe -i shaders/textured_shader.glsl -o source/shaders_code/textured_shader.odin -l glsl430 -f sokol_odin
 .\tools\sokol-shdc.exe -i shaders/textured_points_shader.glsl -o source/shaders_code/textured_points_shader.odin -l glsl430 -f sokol_odin
 .\tools\sokol-shdc.exe -i shaders/base_points_shader.glsl -o source/shaders_code/base_points_shader.odin -l glsl430 -f sokol_odin
